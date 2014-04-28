180fcf47311e48b95c90f4c80b25db109b54fdd7
Buffalo Hird <buffalohird@college.harvard.edu>
2014-04-28 01:31:59 -0400

    added reading closest graphs and etc

--- storage.py
+++ storage.py
@@ -6,7 +6,7 @@ import csv
 
 class Storage:
   def __init__(self):
-    self.hello = []
+    self.runFiles = ['wow.csv', 'wow1.csv', 'wow2.csv']
 
   # given a file name of a csv will read it and return that data when Storage.readRun() is called
   def readRun(self, inputFile):
@@ -26,19 +26,64 @@ class Storage:
       print "IOERROR is raised. Error opening file %s with logger.py" % (str(inputFile)) 
 
 
+  def readRunDistance(self, inputFile):
+    try:
+      with open(inputFile, "rU") as csvFile:
+        csvReader = csv.reader(csvFile)
+        if csvReader == []:
+          print "empty reader trying to read distance of run %s" % (inputFile)
+          time.sleep(100)
+        for row in csvReader:
+          return float(row[0])
+    except IOError:
+      print "IOERROR is raised. Error opening file %s with logger.py" % (str(inputFile)) 
+
+
   # given a list of lists to save and a file name will save csv of lists to name
-  def saveRun(self, outputData, outputFile):
+  def saveRun(self, distance, outputData, outputFile):
     with open(outputFile, 'w+') as writeFile:
       csvWriter = csv.writer(writeFile)
-      for row in self.data:
+      csvWriter.writerow([distance])
+      for row in outputData:
+        print row
         csvWriter.writerow(row)
 
 
+  def findNClosestRuns(self, distance, n):
+    closestRunsList = []
+    for runString in self.runFiles:
+      closestRunsList.append([self.readRunDistance(runString), runString])
+
+    print closestRunsList
+    print distance
+    closestRunsList = sorted(closestRunsList, key=lambda x: abs(x[0]-distance))
+
+    print closestRunsList
+
+    if n > len(closestRunsList):
+      print "error trying to return %d closest graphs" % (n)
+    return closestRunsList[0:n-1]
+
+
+
+
+
+
+
 # example usage
-"""
-storage = Storage()
-print storage.readRun('test1.csv')
-"""
+
+# find the n closest runs to a given distance to use for calculating
+#storage = Storage()
+#x = storage.findNClosestRuns(5000.0, n)
+
+
+# save a run
+#storage = Storage()
+#storage.saveRun(5000.0 #meters, [[x,y][x,y]], 'filename.csv')
+#
+
+
+
 
 
 
