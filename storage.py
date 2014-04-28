ad1d61457393c79467dda21bea791a398c5ab58c
Buffalo Hird <buffalohird@college.harvard.edu>
2014-04-27 22:21:11 -0400

    reading and writing runs!

---
+++ storage.py
@@ -0,0 +1,39 @@
+import time
+import csv
+
+
+
+
+class Storage:
+  def __init__(self):
+    self.hello = []
+
+  def readRun(self, inputFile):
+    try:
+      with open(inputFile, "rU") as csvFile:
+        returnPoints = []
+        csvReader = csv.reader(csvFile)
+
+        if csvReader == []:
+          print "empty reader"
+          time.sleep(100)
+        for row in csvReader:
+          returnPoints.append(row)
+
+        return returnPoints
+    except IOError:
+      print "IOERROR is raised. Error opening file %s with logger.py" % (str(inputFile)) 
+
+
+  def saveRun(self, outputData, outputFile):
+    with open(outputFile, 'w+') as writeFile:
+      csvWriter = csv.writer(writeFile)
+      for row in self.data:
+        csvWriter.writerow(row)
+
+
+storage = Storage()
+print storage.readRun('test1.csv')
+
+
+
